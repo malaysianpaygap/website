79c5a538504b20e6801c7ccbeecd421d340b6a38
“nicolehuey <z5211373@student.unsw.edu.au>
2022-04-19 19:28:18 +0800

    fix: styling

--- src/components/form/searchable-dropdown.tsx
+++ src/components/form/searchable-dropdown.tsx
@@ -57,9 +57,8 @@ export const SearchableDropdown = React.forwardRef<
         >
           <div>
             <input
-              {...getInputProps()}
               className={cls(
-                'px-2.5 py-2.5 rounded-md shadow-sm focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 block min-w-0 w-full sm:text-sm border border-solid border-gray-300 text-left',
+                'px-2.5 py-2.5 rounded-md shadow-sm block min-w-0 w-full sm:text-sm border border-solid border-gray-300 text-left focus:border-primary-300 focus:ring focus:ring-primary-300 focus:ring-opacity-50  outline-transparent outline-0',
                 status ? borderByStatus[status] : 'border-gray-300',
                 inputProps.disabled && 'bg-gray-100 text-gray-400',
                 className
@@ -76,7 +75,7 @@ export const SearchableDropdown = React.forwardRef<
               <ul
                 {...getMenuProps()}
                 className={cls(
-                  'absolute z-10 w-full focus:border-primary-300 focus:ring focus:ring-primary-200 focus:ring-opacity-50 border border-solid border-gray-300 max-h-56 rounded-md text-base overflow-auto sm:text-sm',
+                  'absolute z-10 w-full border border-solid border-gray-300 max-h-56 rounded-md text-base overflow-auto sm:text-sm',
                   status ? borderByStatus[status] : 'border-gray-300',
                   inputProps.disabled && 'bg-gray-100 text-gray-400',
                   className
