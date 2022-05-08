c89eb8d77de994efee129e3b814b74eee735add1
Kelvin Lee <kelvin003@e.ntu.edu.sg>
2022-05-08 20:25:18 +0800

    fix: conciseness

--- src/components/form/searchable-dropdown.tsx
+++ src/components/form/searchable-dropdown.tsx
@@ -2,6 +2,10 @@ import Downshift from 'downshift';
 import * as React from 'react';
 
 import { cls } from '@/lib/clsxm';
+import {
+  filterOptionsByQuery,
+  SearchableDropdownOption,
+} from '@/lib/dropdown-search';
 
 import { borderByStatus } from './border';
 import { useFieldControlContext } from './field-context';
@@ -9,11 +13,7 @@ import { useFieldControlContext } from './field-context';
 export interface SearchableDropdownProps
   extends React.ComponentPropsWithoutRef<'input'> {
   id: string; // id is required for this component to be SSR-friendly
-  options: Array<{
-    label: string;
-    description?: string;
-    value: string;
-  }>;
+  options: Array<SearchableDropdownOption>;
   /**
    * callback to be invoked when input change. The parameter will
    * be the value instead of the event object
@@ -30,7 +30,7 @@ export const SearchableDropdown = React.forwardRef<
 ) {
   const { inputId, status } = useFieldControlContext(id);
 
-  const optionsHaveDescription = options.find((option) => option.description)
+  const optionsHaveDescription = options.some((option) => option.description)
     ? true
     : false;
 
@@ -56,52 +56,45 @@ export const SearchableDropdown = React.forwardRef<
         closeMenu,
         highlightedIndex,
         isOpen,
-      }) => (
-        <div>
+      }) => {
+        const filteredOptions = inputValue
+          ? filterOptionsByQuery(options, inputValue)
+          : options;
+
+        return (
           <div>
-            <input
-              className={cls(
-                'px-2.5 py-2.5 rounded-md shadow-sm block min-w-0 w-full sm:text-sm border border-solid border-gray-300 text-left focus:border-primary-300 focus:ring focus:ring-primary-300 focus:ring-opacity-50  outline-transparent outline-0',
-                status ? borderByStatus[status] : 'border-gray-300',
-                inputProps.disabled && 'bg-gray-100 text-gray-400',
-                className
-              )}
-              placeholder='Please choose'
-              {...getInputProps({
-                ...inputProps,
-                onFocus: openMenu,
-                onBlur: closeMenu,
-                ref: forwardedRef,
-              })}
-            />
-          </div>
-          <div className='relative'>
-            {isOpen && (
-              <div
-                {...getMenuProps()}
+            <div>
+              <input
                 className={cls(
-                  'absolute z-10 w-full border border-solid border-gray-300 max-h-56 rounded-md text-base overflow-auto sm:text-sm bg-white',
-                  optionsHaveDescription && 'divide-y divide-slate-200',
+                  'px-2.5 py-2.5 rounded-md shadow-sm block min-w-0 w-full sm:text-sm border border-solid border-gray-300 text-left focus:border-primary-300 focus:ring focus:ring-primary-300 focus:ring-opacity-50  outline-transparent outline-0',
                   status ? borderByStatus[status] : 'border-gray-300',
                   inputProps.disabled && 'bg-gray-100 text-gray-400',
                   className
                 )}
-                onClick={() => openMenu}
-              >
-                {options.filter(
-                  (item) =>
-                    !inputValue ||
-                    item.value.toLowerCase().includes(inputValue.toLowerCase())
-                ).length > 0 ? (
-                  options
-                    .filter(
-                      (item) =>
-                        !inputValue ||
-                        item.value
-                          .toLowerCase()
-                          .includes(inputValue.toLowerCase())
-                    )
-                    .map((item, index) =>
+                placeholder='Please choose'
+                {...getInputProps({
+                  ...inputProps,
+                  onFocus: openMenu,
+                  onBlur: closeMenu,
+                  ref: forwardedRef,
+                })}
+              />
+            </div>
+            <div className='relative'>
+              {isOpen && (
+                <div
+                  {...getMenuProps()}
+                  className={cls(
+                    'absolute z-10 w-full border border-solid border-gray-300 max-h-56 rounded-md text-base overflow-auto sm:text-sm bg-white',
+                    optionsHaveDescription && 'divide-y divide-slate-200',
+                    status ? borderByStatus[status] : 'border-gray-300',
+                    inputProps.disabled && 'bg-gray-100 text-gray-400',
+                    className
+                  )}
+                  onClick={() => openMenu}
+                >
+                  {filteredOptions.length > 0 ? (
+                    filteredOptions.map((item, index) =>
                       optionsHaveDescription ? (
                         <div
                           className={cls(
@@ -137,16 +130,17 @@ export const SearchableDropdown = React.forwardRef<
                         </div>
                       )
                     )
-                ) : (
-                  <div className='py-2 mx-2 text-gray-500 text-sm'>
-                    No Result
-                  </div>
-                )}
-              </div>
-            )}
+                  ) : (
+                    <div className='py-2 mx-2 text-gray-500 text-sm'>
+                      No Result
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-      )}
+        );
+      }}
     </Downshift>
   );
 });
